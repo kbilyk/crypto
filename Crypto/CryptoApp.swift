af18393ef76c6b4e1212c845f60363eb1bcba00f
Kostiantyn Bilyk <k.bilyk@svitla.com>
2023-01-05 18:28:16 +0200

    Initial Commit

---
+++ Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by Kostiantyn Bilyk on 05.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
