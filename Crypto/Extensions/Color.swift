5a8a4dd5d3324b7bc8242989785eaf6164403e4e
Kostiantyn Bilyk <kos.billyk@gmail.com>
2023-01-11 19:30:12 +0200

    [CA-1] Basic project structure and Color Scheme setup.

---
+++ Crypto/Extensions/Color.swift
@@ -0,0 +1,21 @@
+//
+//  Color.swift
+//  Crypto
+//
+//  Created by Kostiantyn Bilyk on 11.01.2023.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let theme = ColorTheme()
+}
+
+struct ColorTheme {
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextColor")
+}
